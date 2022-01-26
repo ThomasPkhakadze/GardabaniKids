3834f24552b596f6b55822811ad91ea31456f2ff
Thomas420 <thompkhakadze@gmail.com>
2022-01-26 11:33:39 +0400

    main page visual

--- resources/views/front/pages/about.blade.php
+++ resources/views/front/pages/about.blade.php
@@ -1,5 +1,5 @@
 <section class="about-section">
-    <div class="container">
+    <div class="container pt-106">
         <div class="row">
             <div class="col-12 d-flex justify-content-center pt-2">
                 <div class="page-header">
